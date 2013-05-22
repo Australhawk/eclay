6675f2fad874848e050701ce785bc8f8180b1585
Sebastian Paiva <austral@gamerdeal.tv>
2013-05-22 18:27:23 -0400

    Inicial

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+EClay::Application.config.secret_token = '8f0e80949654f4f6d72846f143fa84733e2d319d361fbcda0f0123f012b5c50b08b6170b38884183b718e11760b2cee8a57a4305d92a24f3e2333c7d7773d097'
